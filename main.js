83dbf8bff0307ba9f21db10ebf18842fe9843552
Aamir Khan Channa <160345903+aamirkhanchanna@users.noreply.github.com>
2024-03-06 22:43:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var favouriteColor = "Blue";
+console.log(favouriteColor);
+favouriteColor = "green";
+console.log(favouriteColor);
+var userName = "Aamir Khan Channa";
+console.log(userName);
+var age = 25;
+console.log(age);
+var isStudent = true;
+console.log(isStudent);
---
+++ main.ts
@@ -0,0 +1,11 @@
+let favouriteColor = "Blue";
+console.log(favouriteColor)
+favouriteColor = "green";
+console.log(favouriteColor);
+
+let userName: string = "Aamir Khan Channa";
+console.log(userName)
+let age: number = 25
+console.log(age)
+let isStudent: boolean = true
+console.log(isStudent)
